5667437699d488e15413d86502b68653462c0768
chuongcuu <dangchuong1999@outlook.com>
2020-02-25 14:05:08 +0200

    index.js: ES6 adaptations, cosmetic tweaks.

--- index.js
+++ index.js
@@ -1,53 +1,48 @@
 const expect = require('expect');
 
-function counter(state, action) {
-    if (state === undefined) {
-        return state = 0; //Redux convention: return the default state when passed with state argument of type undefined
-    } else if (action.type === "INCREMENT") {
-        return state += 1
-    } else if (action.type === "DECREMENT") {
-        return state -= 1;
-    } else {
-        return state; //return current state when receiving unknown action
+counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return (state += 1);
+        case 'DECREMENT':
+            return (state -= 1);
+        default:
+            return state;
     }
-}
+};
 
 expect(
     counter(0, {
-        type: "INCREMENT"
+        type: 'INCREMENT'
     })
 ).toEqual(1);
 
-
 expect(
     counter(1, {
-        type: "INCREMENT"
+        type: 'INCREMENT'
     })
 ).toEqual(2);
 
-
 expect(
     counter(2, {
-        type: "DECREMENT"
+        type: 'DECREMENT'
     })
 ).toEqual(1);
 
 expect(
     counter(1, {
-        type: "DECREMENT"
+        type: 'DECREMENT'
     })
 ).toEqual(0);
 
 expect(
-    counter(0, {
-        type: "SOMETHING_ELSE"
+    counter(2, {
+        type: 'SOMETHING_ELSE'
     })
-).toEqual(0);
+).toEqual(2);
 
 expect(
-    counter(undefined, {
-        type: "INCREMENT"
-    })
+    counter(undefined, {})
 ).toEqual(0);
 
-console.log("All tests passed.");
+console.log('All tests passed.');
